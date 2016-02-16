01478c9f36ee27621a63c5145c26179ba2fc8907
Alex Hagge and Ming Wen <alexhagge128@gmail.com>
2016-02-15 21:03:20 -0800

    Added error class

--- js/scripts.js
+++ js/scripts.js
@@ -5,28 +5,21 @@ $(function() {
     var rating3 = parseInt($("input#rating3").val());
 
     var totalRating = rating1 + rating2 + rating3;
-<<<<<<< HEAD
-
-    if (totalRating >= 20) {
-      $('#conservative').show();
-    }
-    else if ((totalRating > 10) && (totalRating < 20)) {
-      $('#moderate').show();
-    }
-    else if (totalRating <= 10) {
-      $('#liberal').show();
-=======
-    console.log("Hello World");
 
     if (totalRating >= 20) {
       $("#rating").text("Conservative");
     }
-    else if ((totalRating > 10) && (totalRating < 20)) {
+    else if (totalRating > 10 && totalRating < 20) {
       $("#rating").text("Moderate");
     }
-    else (totalRating <= 10) {
+
+    else if (totalRating > 3 && totalRating <= 10) {
       $("#rating").text("Liberal");
->>>>>>> 478c3c33c155cabfb2a73271e91f1fc4c349d868
+    }
+
+    else {
+      console.log("stuff");
+      $(".form-control").addClass(".has-error");
     }
 
       event.preventDefault();
